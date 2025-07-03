d9120dfacfa6d246b565e9e1fb702163c6e558f5
LoistaAmanda <145968072+LoistaAmanda@users.noreply.github.com>
2025-07-03 12:27:30 +0700

    upload project movie-app

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Button from "../components/ui/button/index";
+import Button from "../components/UI/Button/index";
 
 
 function DetailMovie() {
